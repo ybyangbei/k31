0453d02d0010b6e69be4fa28829d6c54f6f645ab
ybyangbei <ybyangbei@sina.com>
2021-05-27 00:22:59 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -12,20 +12,20 @@
 sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
 
 # 取消Lean大登陆密码
-#sed -i 's/^\(.*99999\)/#&/' package/lean/default-settings/files/zzz-default-settings
+sed -i 's/^\(.*99999\)/#&/' package/lean/default-settings/files/zzz-default-settings
 
 # 修正Lean大发行版软件源（注释掉原修改）
-sed -i 's/^\(.*downloads.openwrt.org\)/#&/' package/lean/default-settings/files/zzz-default-settings
-sed -i 's/^\(.*http:\)/#&/' package/lean/default-settings/files/zzz-default-settings
-sed -i 's/^\(.*openwrt_luci\)/#&/' package/lean/default-settings/files/zzz-default-settings
+#sed -i 's/^\(.*downloads.openwrt.org\)/#&/' package/lean/default-settings/files/zzz-default-settings
+#sed -i 's/^\(.*http:\)/#&/' package/lean/default-settings/files/zzz-default-settings
+#sed -i 's/^\(.*openwrt_luci\)/#&/' package/lean/default-settings/files/zzz-default-settings
 
 # 修改Lean大发行版软件源（snapshots改成releases/19.07-SNAPSHOT）
 #sed -i 's#snapshots#releases/19.07-SNAPSHOT#g' package/lean/default-settings/files/zzz-default-settings
 
 # 删除lienol大luci缺失插件
-rm -rf feeds/luci/applications/luci-app-https-dns-proxy
-rm -rf feeds/telephony/net/freeswitch-stable
-rm -rf feeds/telephony/net/freeswitch-stable-mod-bcg729
+#rm -rf feeds/luci/applications/luci-app-https-dns-proxy
+#rm -rf feeds/telephony/net/freeswitch-stable
+#rm -rf feeds/telephony/net/freeswitch-stable-mod-bcg729
 
 # 补全lienol大luci源smartdns缺失的插件核心
 svn co https://github.com/Lienol/openwrt-packages/trunk/net/smartdns package/lienol/smartdns
