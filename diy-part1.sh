83df731dbf6a055525be8a0b725eaaea62db8d94
ybyangbei <ybyangbei@sina.com>
2021-05-26 22:37:33 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -1,17 +1,23 @@
 #!/bin/bash
-#
-# Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
-#
-# This is free software, licensed under the MIT License.
-# See /LICENSE for more information.
-#
+#=============================================================
 # https://github.com/P3TERX/Actions-OpenWrt
 # File name: diy-part1.sh
 # Description: OpenWrt DIY script part 1 (Before Update feeds)
-#
+# Lisence: MIT
+# Author: P3TERX
+# Blog: https://p3terx.com
+#=============================================================
 
 # Uncomment a feed source
+# 取消helloworld源注释（ssr-plus）
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+# 取消telephony源注释
+sed -i 's/^#\(.*telephony\)/\1/' feeds.conf.default
+
+# comment a feed source
+# 注释掉原lean大luci源
+sed -i 's/^\(.*luci\)/#&/' feeds.conf.default
 
 # Add a feed source
-#sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+# 添加lienol大luci源
+#esed -i '$a src-git luci https://github.com/Lienol/openwrt-luci.git;dev-18.06' feeds.conf.default
